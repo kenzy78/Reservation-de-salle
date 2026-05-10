2fb170de1800365b85ceda90899c02781482dcb3
Kenzy henry <ton-email@exemple.com>
2026-05-10 18:07:45 +0200

    Refactor CSS and HTML structure; remove old styles, add new styles and logo images

--- index.php
+++ index.php
@@ -6,13 +6,119 @@
 </head>
     <body>
         <header class="header">
-            <div class=container>
-                <span class= "tittle">CHOP TA SSALLE.COM<span>
-                <span class= "desc"> site de réservation de salle</span>
-                <
-            </div>
+                <img src="./image/logo.jpg" alt="logo d'entreprise"  
+            <nav>
+                <ul class="border">
+                    <li><a href="acceuille.html">Acceuille</a></li>
+                    <li><a href="reservation.php">Produit</a></li>
+                    <li><a href="contact.html">contact</a></li>
+                    <li><a href="confirmation.php">reservation</a></li>
+                <ul>
+            </nav>
         </header>
-        <div>
-            
+
+        <main>
+<section class="sgrille">
+     <div class="grille">
+            <div class="product"> 
+                <div class="image">
+                    <img src="./image/salle1.jpg" alte="salle" width="200px" height="200px">                
+                </div>           
+                <div class="textes">
+                    <h4 class="productName">Sallle</h4>
+                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing
+                    <h5 class="prix">1500e</h5>
+                </div>
+                <div class="boutons">
+                    <button class="details">détaills</button> 
+                    <button class="ajouter">réserver</bbutton>
+                </div>
+
+            </div>
+
+             <div class="product"> 
+                <div class="image">
+                    <img src="./image/salle1.jpg" alte="salle" width="200px" height="200px">                
+                </div>           
+                <div class="textes">
+                    <h4 class="productName">Sallle</h4>
+                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing
+                    <h5 class="prix">1500e</h5>
+                </div>
+                <div class="boutons">
+                    <button class="details">détaills</button> 
+                    <button class="ajouter">réserver</bbutton>
+                </div>
+
+            </div>
+
+             <div class="product"> 
+                <div class="image">
+                    <img src="./image/salle1.jpg" alte="salle" width="200px" height="200px">                
+                </div>           
+                <div class="textes">
+                    <h4 class="productName">Sallle</h4>
+                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing
+                    <h5 class="prix">1500e</h5>
+                </div>
+                <div class="boutons">
+                    <button class="details">détaills</button> 
+                    <button class="ajouter">réserver</bbutton>
+                </div>
+
+                 <div class="product"> 
+                <div class="image">
+                    <img src="./image/salle1.jpg" alte="salle" width="200px" height="200px">                
+                </div>           
+                <div class="textes">
+                    <h4 class="productName">Sallle</h4>
+                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing
+                    <h5 class="prix">1500e</h5>
+                </div>
+                <div class="boutons">
+                    <button class="details">détaills</button> 
+                    <button class="ajouter">réserver</bbutton>
+                </div>
+
+            </div>
+
+             <div class="product"> 
+                <div class="image">
+                    <img src="./image/salle1.jpg" alte="salle" width="200px" height="200px">                
+                </div>           
+                <div class="textes">
+                    <h4 class="productName">Sallle</h4>
+                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing
+                    <h5 class="prix">1500e</h5>
+                </div>
+                <div class="boutons">
+                    <button class="details">détaills</button> 
+                    <button class="ajouter">réserver</bbutton>
+                </div>
+
+            </div>
+
+             <div class="product"> 
+                <div class="image">
+                    <img src="./image/salle1.jpg" alte="salle" width="200px" height="200px">                
+                </div>           
+                <div class="textes">
+                    <h4 class="productName">Sallle</h4>
+                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing
+                    <h5 class="prix">1500e</h5>
+                </div>
+                <div class="boutons">
+                    <button class="details">détaills</button> 
+                    <button class="ajouter">réserver</bbutton>
+                </div>
+
+            </div>
+        </div>
+</section>
+
+
+        </main>
+        <footer>
+        </footer>
     </body>
 </html>
---
+++ reservation.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+                <img src="./image/logo.jpg" alt="logo d'entreprise"  
+            <nav>
+                <ul class="border">
+                    <li><a href="acceuille.html">Acceuille</a></li>
+                    <li><a href="reservation.php">Produit</a></li>
+                    <li><a href="contact.html">contact</a></li>
+                    <li><a href="confirmation.php">reservation</a></li>
+                <ul>
+            </nav>
+        </header>
+    <main>
+    </main>
+    
+    <footer>
+    </footer>
+</body>
+</html>
