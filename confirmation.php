0f4bb6d70684bf6143cfea0bc18456ecb2402b6a
Kenzy henry <ton-email@exemple.com>
2026-05-12 14:31:09 +0200

    display to reservation confirmation query

--- confirmation.php
+++ confirmation.php
@@ -6,7 +6,7 @@ $reservation = null;
 
 if ($id > 0) {
 $stmt = $pdo->prepare("
-    SELECT r.*, s.nom AS nom_salle,
+    SELECT r.*, s.nom AS nom_salle, s.peix
     FROM `réservation` r
     JOIN `salle` s ON r.id_salle = s.id_salle
     WHERE r.`id_réservation` = ?
